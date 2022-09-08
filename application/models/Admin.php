9651dd6a50a19f58f2ba486b312d37dd2b363c04
goodkey24 <70890450+goodkey24@users.noreply.github.com>
2022-09-08 15:11:46 +0700

    Create Admin.php

---
+++ application/models/Admin.php
@@ -0,0 +1,53 @@
+<?php
+if (!defined('BASEPATH')) {
+  exit('No direct script access allowed');
+}
+
+class Admin extends CI_Model
+{
+  public function __construct()
+  {
+    parent::__construct();
+  }
+
+  public function get_data($adminid = "")
+  {
+    $this->db->select('admin.*,
+            ')->where_in('level', ['admin']);
+    $this->db->from('admin');
+    if ($adminid != "") {
+      $this->db->where('admin.id', $adminid);
+    }
+    $this->db->order_by('admin.id', 'desc');
+    $query = $this->db->get();
+    if ($query->num_rows() > 0) {
+      if ($adminid == "") {
+        return $query->result_array();
+      } else {
+        return $query->row_array();
+      }
+    } else {
+      return null;
+    }
+  }
+
+  public function find_admin($username, $id = "")
+  {
+    $this->db->select('
+        admin.*
+     ');
+    $this->db->from('admin');
+    $this->db->where('admin.username', $username);
+
+    if ($id != "") {
+      $this->db->where_not_in('admin.id', $id);
+    }
+
+    $query = $this->db->get();
+    if ($query->num_rows() > 0) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+}
