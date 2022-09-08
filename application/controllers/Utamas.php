fc7eb29d7dfed129986439e034844b5366b07041
goodkey24 <70890450+goodkey24@users.noreply.github.com>
2022-09-08 15:11:36 +0700

    Update Utamas.php

--- application/controllers/Utamas.php
+++ application/controllers/Utamas.php
@@ -7,12 +7,23 @@ class Utamas extends CI_Controller
   public function __construct()
   {
     parent::__construct();
+    cek_session('admin');
+    $this->load->model(array('utama'));
   }
 
   public function index()
   {
-    $data['title'] = "Dashboard Admin Kanwil";
-    $this->template->view('template', 'home', $data);
+    if ($this->session->userdata('level') == 'admin') {
+
+      $data['title'] = "Dashboard Admin";
+      $this->template->view('template', 'homeadmin', $data);
+    } else {
+
+      $adminid            = $this->session->userdata('adminid');
+      $result         = $this->utama->get_data($adminid);
+      $data['result'] = $result;
+      $this->template->view('template', 'profil/profil', $data);
+    }
   }
 }
 
